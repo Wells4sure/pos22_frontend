3ee0afd7f86bae0265c01b5638a4844505968fa7
Wellington <Wellington.Chanda@zamtel.co.zm>
2022-03-18 14:18:08 +0200

    cards

---
+++ src/views/Dashboard/CardItem.tsx
@@ -0,0 +1,88 @@
+import { Box, Card, CardContent, Typography, useTheme } from "@mui/material";
+import { ShoppingCart as ShoppingCartIcon } from "@mui/icons-material";
+import { textAlign } from "@mui/system";
+import CountUp from "react-countup";
+
+export interface props {
+  cardColor: string;
+  cardTitle: string;
+}
+const CardItem = ({ cardColor, cardTitle }: props) => {
+  const theme = useTheme();
+  const colors = [
+    {
+      name: "black",
+      value:
+        "rgba(0,0,0,0) linear-gradient(195deg, rgb(66,66,74), rgb(25,25,25)) repeat scroll 0% 0%",
+    },
+    {
+      name: "green",
+      value:
+        "rgba(0,0,0,0) linear-gradient(195deg, rgb(102,187,106), rgb(67,160,71)) repeat scroll 0% 0%",
+    },
+    {
+      name: "blue",
+      value:
+        "rgba(0,0,0,0) linear-gradient(195deg, rgb(73, 163, 241), rgb(26, 115, 232)) repeat scroll 0% 0%",
+    },
+    {
+      name: "red",
+      value:
+        "rgba(0, 0, 0, 0) linear-gradient(195deg, rgb(236, 64, 122), rgb(216, 27, 96)) repeat scroll 0% 0%",
+    },
+  ];
+  const cl = colors.find((element) => {
+    if (element.name === cardColor) {
+      return element.value;
+    }
+  });
+
+  return (
+    <Card>
+      <Box sx={{ display: "flex", flexDirection: "row" }}>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            opacity: 1,
+            background: cl?.value,
+            color: "rgb(255, 255, 255)",
+            borderRadius: "2px ,0, 2px ,0",
+            width: 100,
+          }}
+        >
+          <ShoppingCartIcon sx={{ height: 38, width: 38 }} />
+        </Box>
+        <CardContent
+          sx={{ flex: "1 0 auto", justifyContent: "right", textAlign: "right" }}
+        >
+          <Typography
+            variant="subtitle1"
+            color="text.secondary"
+            component="div"
+          >
+            {cardTitle}
+          </Typography>
+          <Typography
+            component="div"
+            variant="h4"
+            sx={{ fontWeight: "700", fontSize: "1.5rem" }}
+          >
+            <CountUp
+              start={0.0}
+              end={1285.5}
+              duration={2.75}
+              separator=" "
+              decimals={2}
+              decimal="."
+              prefix="K "
+            />
+          </Typography>
+        </CardContent>
+      </Box>
+    </Card>
+  );
+};
+
+export default CardItem;
--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -1,9 +1,85 @@
-import React from 'react'
+import {
+  Container,
+  Grid,
+  List,
+  ListItemButton,
+  ListItemText,
+  ListSubheader,
+  Paper,
+} from "@mui/material";
+import React from "react";
+import CardItem from "./CardItem";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <Container maxWidth="xl" sx={{ mt: 4, mb: 4 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={3}>
+          <CardItem cardTitle="Today's Sales" cardColor="green" />
+        </Grid>
+        <Grid item xs={12} md={3}>
+          <CardItem cardTitle="Yesterday's Sales" cardColor="black" />
+        </Grid>
+        <Grid item xs={12} md={3}>
+          <CardItem cardTitle="This Month's Sales" cardColor="blue" />
+        </Grid>
+        <Grid item xs={12} md={3}>
+          <CardItem cardTitle="Last Month's Sales" cardColor="red" />
+        </Grid>
+        {/* Chart */}
+        <Grid item xs={12} md={8} lg={9}>
+          <Paper
+            sx={{
+              p: 2,
+              display: "flex",
+              flexDirection: "column",
+              height: 240,
+            }}
+          >
+            CHat
+          </Paper>
+        </Grid>
+        {/* Recent Deposits */}
+        <Grid item xs={12} md={4} lg={3}>
+          <Paper
+            sx={{
+              p: 2,
+              display: "flex",
+              flexDirection: "column",
+              height: 240,
+            }}
+          >
+            <List
+              sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+              component="nav"
+              aria-labelledby="nested-list-subheader"
+              subheader={
+                <ListSubheader component="div" id="nested-list-subheader">
+                  Top Products
+                </ListSubheader>
+              }
+            >
+              <ListItemButton
+                sx={{ display: "flex", justifyContent: "space-between" }}
+              >
+                <ListItemText primary="Mosi" />
+                <ListItemText secondary="K 1000" />
+              </ListItemButton>
+              <ListItemButton>
+                <ListItemText primary="Drafts" />
+              </ListItemButton>
+            </List>
+          </Paper>
+        </Grid>
+        {/* Recent Orders */}
+        <Grid item xs={12}>
+          <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+            Recent Orders
+          </Paper>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
