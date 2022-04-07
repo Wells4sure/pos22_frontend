00ba7d05fe2ab9740850bed81c93e253c6063abf
Wellington <Wellington.Chanda@zamtel.co.zm>
2022-04-07 07:24:34 +0200

    pos responsive | functionality

--- src/views/Pos/CartActions.tsx
+++ src/views/Pos/CartActions.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Box,
   Button,
@@ -12,6 +12,18 @@ import {
 } from "@mui/material";
 
 const CartActions = () => {
+  const [paymentMethod, setPaymentMethod] = useState("");
+  const [amountPaid, setAmountPaid] = useState(0);
+  const [otherMode, setOtherMode] = useState("Not Specific");
+
+  // List of payment methods
+  const paymentMethods = [
+    "Cash",
+    "Mobile Money",
+    "Credit Card",
+    "Cheque",
+    "Other",
+  ];
   return (
     <Grid container spacing={2} sx={{ my: 1 }}>
       <Grid
@@ -48,24 +60,62 @@ const CartActions = () => {
       </Grid>
 
       <Grid item xs={6} sx={{ p: 1, border: "1px dashed grey" }}>
-        <FormControl fullWidth>
+        <FormControl fullWidth size="small">
           <InputLabel id="demo-simple-select-label">Add Payment</InputLabel>
           <Select
             labelId="demo-simple-select-label"
             id="demo-simple-select"
             label="Add Payment"
+            value={paymentMethod}
+            onChange={(e) => setPaymentMethod(e.target.value)}
           >
-            <MenuItem value={10}>Cash</MenuItem>
-            <MenuItem value={20}>Cheque</MenuItem>
-            <MenuItem value={30}>Credit Card</MenuItem>
-            <MenuItem value={40}>Mobile Money</MenuItem>
-            <MenuItem value={50}>Other</MenuItem>
+            <MenuItem value="">Select Payment Method</MenuItem>
+            {paymentMethods.map((paymentMethod, index) => (
+              <MenuItem key={index} value={paymentMethod}>
+                {paymentMethod}
+              </MenuItem>
+            ))}
           </Select>
         </FormControl>
       </Grid>
       <Grid item xs={6} sx={{ p: 1, border: "1px dashed grey" }}>
-        <FormControl fullWidth>
-          <TextField id="outlined-basic" label="Amount Paid" variant="outlined" />
+        <FormControl
+          fullWidth
+          sx={{ display: paymentMethod === "Other" ? "none" : "block" }}
+        >
+          <TextField
+            id="outlined-basic"
+            type="number"
+            label="Amount Paid"
+            variant="outlined"
+            size="small"
+            value={amountPaid}
+            onChange={(e) => {
+              setAmountPaid(Number(e.target.value));
+            }}
+            disabled={
+              paymentMethod === "" ||
+              paymentMethod === "Other" ||
+              paymentMethod !== "Cash"
+            }
+          />
+        </FormControl>
+        {/* // Other */}
+        <FormControl
+          fullWidth
+          sx={{ display: paymentMethod === "Other" ? "block" : "none" }}
+        >
+          <TextField
+            id="outlined-basic"
+            label="Specify Other"
+            variant="outlined"
+            size="small"
+            value={otherMode}
+            onChange={(e) => {
+              setOtherMode(e.target.value);
+            }}
+            disabled={paymentMethod === "" || paymentMethod !== "Other"}
+          />
         </FormControl>
       </Grid>
       <Grid
@@ -78,13 +128,13 @@ const CartActions = () => {
           border: "1px dashed grey",
         }}
       >
-        <Button variant="contained" color="error">
+        <Button variant="contained" color="error" size="small">
           Cancel
         </Button>{" "}
-        <Button variant="contained" color="secondary">
+        <Button variant="contained" color="secondary" size="small">
           Hold
         </Button>
-        <Button size="large" variant="contained" color="primary">
+        <Button variant="contained" color="primary" size="small">
           Complete Sale
         </Button>
       </Grid>
--- src/views/Pos/TouchScreen.tsx
+++ src/views/Pos/TouchScreen.tsx
@@ -5,39 +5,69 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, useMediaQuery } from "@mui/material";
 import defaultProductImage from "../../assets/images/products.png";
+import theme from "../../Theme";
 
 export default function TouchScreen() {
+  const matches = useMediaQuery(theme.breakpoints.up("xs"));
   return (
     <Box
       sx={{
         m: "10px 4px",
-        height: "650px",
+        height: matches ? "650px" : "300px",
         overflowX: "scroll",
         overflowY: "none",
       }}
     >
       <Grid container spacing={2} sx={{ gap: 0 }}>
         {Array.from(Array(20)).map((_, index) => (
-          <Grid item xs={12} md={3} lg={3}>
+          <Grid
+            item
+            sx={{
+              "& :active": {
+                background: "#34ff99",
+              },
+            }}
+            xs={6}
+            md={3}
+            lg={2}
+            key={index}
+          >
             <Card
               sx={{
                 maxHeight: 210,
-                maxWidth: 160,
+                maxWidth: 130,
                 textAlign: "center",
                 cursor: "pointer",
               }}
             >
               <CardMedia
                 component="img"
-                height="140"
+                sx={{
+                  maxWidth: 130,
+                  maxHeight: 130,
+                  p: "5px",
+                }}
+                title="Product Name | Description"
                 image={defaultProductImage}
                 alt="default Product Image"
               />
-              <CardContent sx={{ backgroundColor: "#eee" }}>
-                <Typography component="p">Compound -D</Typography>
-                <Typography component="p">Zambia Fertilizer</Typography>
+              <CardContent
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  maxHeight:80,
+                  backgroundColor: "#e5e5e5",
+                  overflow: "hidden",
+                  whiteSpace: " pre-wrap",
+                  fontSize: 12,
+                  fontWeight: 400,
+                  lineheight: 1.4,
+                }}
+              >
+                <span>Compound -D</span>
+                <span>Zambia Fertilizer</span>
               </CardContent>
             </Card>
           </Grid>
